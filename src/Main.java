68b46a128a3f6aa252f49fe34e954927b29ae340
Rostislav-M <rostislav.malatak@gmail.com>
2024-09-30 14:05:37 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,17 @@ public class Main {
         prodejceMrkve1.setSPZ("RSA CAR04");
         prodejceMrkve1.setSpotrebavozidla(10);
 
-        prodejceMrkve1.setJmeno("Karel Jedlinka");
-        prodejceMrkve1.setAdresa("Praha 4, Budejovicka 1523/9a");
+        prodejceMrkve2.setJmeno("Karel Jedlinka");
+        prodejceMrkve2.setAdresa("Praha 4, Budejovicka 1523/9a");
         prodejceMrkve2.setDatumnarozeni(LocalDate.of(1989,6,11));
         prodejceMrkve2.setPocetsmluv(7);
-        prodejceMrkve2.setMnozstvimrkve(5);
+        prodejceMrkve2.setMnozstvimrkve(9);
         prodejceMrkve2.setNazevmesta("Praha");
         prodejceMrkve2.setSPZ("SMX 17819");
         prodejceMrkve2.setSpotrebavozidla(9);
 
         System.out.println("První prodejce: " + "\n" + "Jmeno: " + prodejceMrkve1.getJmeno() + "\n" + "Cela adresa: " + prodejceMrkve1.getAdresa() + "\n" + "Datum narození: " + prodejceMrkve1.getDatumnarozeni() + "\n" + "Pocet dosud sjednanych smluv: " +  prodejceMrkve1.getPocetsmluv() + "\n" + "Celkove mnozstvi prodane mrkve v tunach: " + prodejceMrkve1.getMnozstvimrkve() + "\n" + "Nazev mesta kde prodejce sidli : " + prodejceMrkve1.getNazevmesta()+ "\n" + "Registracni znacka vozidla: " + prodejceMrkve1.getSPZ() + "\n" + "Spotreba firemniho vozidla v litech na 100km: " + prodejceMrkve1.getSpotrebavozidla() +"\n");
-        System.out.println("Druhy prodejce: " + "\n" + "Jmeno: " + prodejceMrkve1.getJmeno() + "\n" + "Cela adresa: " + prodejceMrkve1.getAdresa() + "\n" + "Datum narození: " + prodejceMrkve2.getDatumnarozeni() + "\n" + "Pocet dosud sjednanych smluv: " +  prodejceMrkve2.getPocetsmluv() + "\n" + "Celkove mnozstvi prodane mrkve v tunach: " + prodejceMrkve2.getMnozstvimrkve() + "\n" + "Nazev mesta kde prodejce sidli : " + prodejceMrkve2.getNazevmesta()+ "\n" + "Registracni znacka vozidla: " + prodejceMrkve2.getSPZ() + "\n" + "Spotreba firemniho vozidla v litech na 100km: " + prodejceMrkve2.getSpotrebavozidla());
+        System.out.println("Druhy prodejce: " + "\n" + "Jmeno: " + prodejceMrkve2.getJmeno() + "\n" + "Cela adresa: " + prodejceMrkve2.getAdresa() + "\n" + "Datum narození: " + prodejceMrkve2.getDatumnarozeni() + "\n" + "Pocet dosud sjednanych smluv: " +  prodejceMrkve2.getPocetsmluv() + "\n" + "Celkove mnozstvi prodane mrkve v tunach: " + prodejceMrkve2.getMnozstvimrkve() + "\n" + "Nazev mesta kde prodejce sidli : " + prodejceMrkve2.getNazevmesta()+ "\n" + "Registracni znacka vozidla: " + prodejceMrkve2.getSPZ() + "\n" + "Spotreba firemniho vozidla v litech na 100km: " + prodejceMrkve2.getSpotrebavozidla());
 
 
     }
